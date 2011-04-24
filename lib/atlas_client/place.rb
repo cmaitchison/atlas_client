988bacdf5020cb9c6e6a92ee53e53083d36b695e
Chris Aitchison <cmaitchison@gmail.com>
2011-04-24 22:16:38 +1000

    Added ability to find Place by name and interrogate its json

--- lib/atlas_client/place.rb
+++ lib/atlas_client/place.rb
@@ -1,7 +1,27 @@
 module AtlasClient
-  class Place
-    def name
-      "My Name"
+  class  Place
+    require 'json'
+    require 'net/http'
+    
+    @url = "http://lposfusion.load.lpo:3000/places"
+    
+    def self.find id
+      response = Net::HTTP.get URI.parse(@url+"?where[id]=#{id}")
+      json = JSON.parse response
+      place_json = json['places'][0]
+      Place.new place_json
+    end
+    
+    def initialize json
+     @json = json
+    end
+  
+    def method_missing(name, *args, &block)
+      @json[name.to_s]
+    end
+    
+    def json
+      @json
     end
   end
 end
